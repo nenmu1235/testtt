6388d9ea93024158ecca3bdd56241eb3eda2b71c
nenmu1235 <127475969+nenmu1235@users.noreply.github.com>
2023-03-16 19:28:48 +0900

    Add files via upload

---
+++ chatbot_model4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 19:26:58 2023
+
+@author: haeyuel
+"""
+
+import openai
+import googletrans
+import streamlit
+
+model_engine = 'davinci'
+openai.api_key = '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'
+
+def generate_text(prompt):
+    response = openai.Completion.create(
+      engine=model_engine,
+      prompt=prompt,
+      max_tokens=20,
+      n=1,
+      stop="STOP",
+      temperature=0.5,
+    )
+    raw_text = response.choices[0].text
+    text = raw_text.strip()
+    return text
+
+def translate(text, lan):
+    translator = googletrans.Translator()
+    response = translator.translate(text, dest = lan).text
+    return response
+def ko_to_en(text):
+    ja_text = translate(text, "ja")
+    en_text = translate(ja_text, "en")
+    return en_text
+def en_to_ko(text):
+    ja_text = translate(text, "ja")
+    ko_text = translate(ja_text, "ko")
+    return ko_text
+
+try:
+    text = st.text_input("대화를 입력하세요: ")
+    en_text = ko_to_en(text)
+    response = generate_text(en_text)
+    ko_text = en_to_ko(response)
+    st.write("당신: ",text)
+    st.write("Chatbot: ", ko_text)
+except:
+    pass
