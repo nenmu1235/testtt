42b504bca41b430cf8a52985584514f5ea8d1b61
nenmu1235 <127475969+nenmu1235@users.noreply.github.com>
2023-03-17 13:08:51 +0900

    Update chatbot_model4.py

--- chatbot_model4.py
+++ chatbot_model4.py
@@ -11,7 +11,7 @@ import googletrans
 import streamlit as st
 
 model_engine = 'davinci'
-openai.api_key = API_KEY
+openai.api_key = st.secrets['API_KEY']
 
 def generate_text(prompt):
     response = openai.Completion.create(
