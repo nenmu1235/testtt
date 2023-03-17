052450245f7415a1a3debfb4c22a61fde79ed5b9
nenmu1235 <127475969+nenmu1235@users.noreply.github.com>
2023-03-17 12:54:03 +0900

    Update chatbot_model4.py

--- chatbot_model4.py
+++ chatbot_model4.py
@@ -39,12 +39,10 @@ def en_to_ko(text):
     ko_text = translate(ja_text, "ko")
     return ko_text
 
-try:
-    text = st.text_input("대화를 입력하세요: ")
-    en_text = ko_to_en(text)
-    response = generate_text(en_text)
-    ko_response = en_to_ko(response)
-    st.write("당신: ",text)
-    st.write("Chatbot: ", ko_response)
-except:
-    pass
+text = st.text_input("대화를 입력하세요: ")
+en_text = ko_to_en(text)
+response = generate_text(en_text)
+ko_response = en_to_ko(response)
+st.write("당신: ",text)
+st.write("Chatbot: ", ko_response)
+
