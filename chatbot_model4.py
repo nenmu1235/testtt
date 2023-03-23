499cba65aaf3f11ade2d0ed222cc6135c9071420
nenmu1235 <127475969+nenmu1235@users.noreply.github.com>
2023-03-24 02:51:00 +0900

    Update chatbot_model4.py

--- chatbot_model4.py
+++ chatbot_model4.py
@@ -46,3 +46,24 @@ ko_response = en_to_ko(response)
 st.write("당신: ",text)
 st.write("Chatbot: ", ko_response)
 
+#추가
+word_list = ['Bullying', 'bully', 'bullied', 'bullies', 'Harassment', 'harass', 'harassed', 'harasses', 'Intimidation', 'intimidate', 'intimidated', 'intimidates', 'Verbal abuse', 'verbally abuse', 'verbally abused', 'verbally abuses', 'Physical abuse', 'physically abuse', 'physically abused', 'physically abuses', 'Cyberbullying', 'cyberbully', 'cyberbullied', 'cyberbullies', 'Social exclusion', 'exclude', 'excluded', 'excludes', 'Victim', 'victimize', 'victimized', 'victimizes', 'Aggressor', 'aggress', 'aggressed', 'aggresses', 'Peer pressure', 'pressure', 'pressured', 'pressures', 'Hazing', 'haze', 'hazed', 'hazes', 'Name-calling', 'call names', 'called names', 'calls names', 'Taunting', 'taunt', 'taunted', 'taunts', 'Threatening', 'threaten', 'threatened', 'threatens', 'Blackmail', 'blackmail', 'blackmailed', 'blackmails', 'Coercion', 'coerce', 'coerced', 'coerces', 'Homophobia', 'homophobe', 'homophobic', 'homophobes', 'Xenophobia', 'xenophobe', 'xenophobic', 'xenophobes', 'Racism', 'racist', 'racial', 'racists', 'Discrimination', 'discriminate', 'discriminated', 'discriminates', 'Bias', 'biased', 'biasing', 'biases', 'Stereotyping', 'stereotype', 'stereotyped', 'stereotypes', 'Prejudice', 'prejudiced', 'prejudicing', 'prejudices', 'Inequality', 'unequal', 'inequality', 'inequalities', 'Oppression', 'oppress', 'oppressed', 'oppresses', 'Power dynamics', 'power', 'powered', 'powers', 'Bullying prevention', 'prevent bullying', 'prevented bullying', 'prevents bullying', 'Conflict resolution', 'resolve conflict', 'resolved conflict', 'resolves conflict', 'Restorative justice', 'restore justice', 'restored justice', 'restores justice', 'Counseling', 'counsel', 'counseled', 'counsels', 'Mental health', 'mental', 'health', 'mental health', 'Trauma', 'traumatize', 'traumatized', 'traumatizes', 'Anxiety', 'anxious', 'anxiety', 'anxieties', 'Depression', 'depress', 'depressed', 'depresses', 'Suicide', 'suicidal', 'suicide', 'suicides', 'Self-harm', 'self-harm', 'self-harmed', 'self-harms', 'Substance abuse', 'abuse substances', 'abused substances', 'abuses substances', 'Academic performance', 'perform academically', 'performed academically', 'performs academically', 'Attendance', 'attend', 'attended', 'attends', 'Truancy', 'truant', 'truanted', 'truants', 'Dropouts', 'dropout', 'dropped out', 'dropouts', 'Suspension', 'suspend', 'suspended', 'suspends', 'Expulsion', 'expel', 'expelled', 'hit', 'violence']
+if en_text.endswith("?"):
+    enc_text = en_text.strip("?")
+else:
+    enc_text = en_text
+for item in enc_text.split():
+    if item in word_list:
+        SMTP_SERVER = 'smtp.gmail.com'
+        SMTP_PORT = 465
+        smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+        EMAIL_ADDR = 'testu7295'
+        EMAIL_PASSWORD = 'gawakfxfrkfmsmtr'
+        smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
+        message = EmailMessage()
+        message.set_content('이메일 본문')
+        message["Subject"] = "warning from chatbot"
+        message["From"] = EMAIL_ADDR  #보내는 사람의 이메일 계정
+        message["To"] = 'testu7295@gmail.com'
+        smtp.send_message(message)
+        smtp.quit()
