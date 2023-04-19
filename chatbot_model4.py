7c269664e81723d24f73c7acbbbcec1ca7924905
nenmu1235 <127475969+nenmu1235@users.noreply.github.com>
2023-04-19 12:33:33 +0900

    Update chatbot_model4.py

--- chatbot_model4.py
+++ chatbot_model4.py
@@ -1,5 +1,4 @@
 import openai
-import googletrans
 import streamlit as st
 import smtplib
 import time
@@ -19,24 +18,26 @@ def generate_text(prompt):
     text = response['choices'][0]['message']['content']
     return text
 
-def translate(text, lan):
-    translator = googletrans.Translator()
-    response = translator.translate(text, dest = lan).text
-    return response
-def ko_to_en(text):
-    ja_text = translate(text, "ja")
-    en_text = translate(ja_text, "en")
-    return en_text
-def en_to_ko(text):
-    ja_text = translate(text, "ja")
-    ko_text = translate(ja_text, "ko")
-    return ko_text
+
+
+if 'chat' not in st.session_state:
+    st.session_state.chat = ''
+
+def submit():
+    st.session_state.chat = st.session_state.widget
+    st.session_state.widget = ''
+
+
+st.write(f'당신: {st.session_state.chat}')
+
+
+
 
 name = st.text_input('이름:')
-text = st.text_input("대화를 입력하세요: ")
+text = st.text_input('대화를 입력하세요', key='widget', on_change=submit)
 response = generate_text(text)
-st.write("당신: ",text)
-st.write("Chatbot: ", response)
+st.write(f'당신: {st.session_state.chat}')
+st.write("토닥이: ", response)
 
 #추가
 word_list = ['괴롭힘','괴롭', '협박', '폭력', '차별', '성희롱', '가해자', '피해자', '사이버 폭력', '인신공격', '피해자 지원', '피해자보호 정책', '선생님 폭력', '학교폭력 보고서', '학교폭력 증거보존', '학교폭력 법적 대응', '선도교사', '학교 내 고립', '학교 내 대인관계', '권리 침해', '가정 내 폭력', '부모교육', '학교폭력 예방 봉사활동', '초등학교 폭력', '중학교 폭력', '고등학교 폭력', '대학교 폭력', '초등학교 폭력 예방', '중학교 폭력 예방', '고등학교 폭력 예방', '대학교 폭력 예방', '학생회 폭력', '체육 대회 폭력', '학교 내 성소수자 차별', '학교 폭력 피해 심리', '학교폭력 관련 법률', '학교폭력 예방 위원회', '학교폭력 예방 자료', '학교폭력 예방 가이드라인', '학교폭력 예방 사례', '불완전한 가정']
